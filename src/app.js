9a5f4de45b084cbea33053ceddd14c5ed8772c77
qlovy <qlovy@sysnet.ch>
2026-04-04 22:53:51 +0200

    add automatic button creation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log("Hello from App.js")
+const nbBox = 6;
+const container = document.getElementById("list-workout");
+for (let i = 1; i < nbBox; i++) {
+    const button = document.createElement('button');
+    button.classList.add('btn','btn-primary', 'my-3');
+    button.innerText = 'Workout ' + i;
+    button.onclick = () => {console.log(i)}
+    container.appendChild(button);
+}
