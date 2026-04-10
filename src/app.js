00b3978d38a577068dfa3dc4af55710f9d352367
qlovy <qlovy@sysnet.ch>
2026-04-10 11:58:42 +0200

    add settings page (style), add download example file.

--- src/app.js
+++ src/app.js
@@ -7,28 +7,37 @@ function init() {
 
 function displayMenu(workouts) {
     const main = document.getElementById('main-content');
-    main.innerHTML = `<div class="text-center py-2">
-      <h1>MMA in pure JS</h1>
+    main.innerHTML = `
+    <div id="secondary-content">
+        <div class="text-center py-2">
+          <h1>MMA in pure JS</h1>
+        </div>
+        <div id="list-workouts" class="d-grid gap-2 py-2 w-75 mx-auto"></div>
     </div>
-
-    <div id="list-workouts" class="d-grid gap-2 py-2 w-75 mx-auto">
-    </div>
-
     <footer class="container text-center fixed-bottom py-4">
       <div class="row">
         <div class="col">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-graph-up-arrow" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-graph-up-arrow" viewBox="0 0 16 16" id="menu-btn">
             <path fill-rule="evenodd" d="M0 0h1v15h15v1H0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5"/>
           </svg>
         </div>
         <div class="col">
-          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-sliders2" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-sliders2" viewBox="0 0 16 16" id="settings-btn">
             <path fill-rule="evenodd" d="M10.5 1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4H1.5a.5.5 0 0 1 0-1H10V1.5a.5.5 0 0 1 .5-.5M12 3.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m-6.5 2A.5.5 0 0 1 6 6v1.5h8.5a.5.5 0 0 1 0 1H6V10a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5M1 8a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 1 8m9.5 2a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V13H1.5a.5.5 0 0 1 0-1H10v-1.5a.5.5 0 0 1 .5-.5m1.5 2.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
           </svg>
         </div>
       </div>
     </footer>`
     addButton(workouts, main);
+    const menuBtn = document.getElementById('menu-btn');
+    menuBtn.addEventListener('click', () => {
+        displayMenu(workouts);
+    });
+    const settingsBtn = document.getElementById('settings-btn');
+    const secondary = document.getElementById('secondary-content');
+    settingsBtn.addEventListener('click', () => {
+        displaySettings(secondary);
+    });
 }
 
 function addButton(workouts, main) {
@@ -44,6 +53,30 @@ function addButton(workouts, main) {
         };
         container.appendChild(button);
     }
+}
+
+function displaySettings(secondary){
+    // TODO: import JSON (user-one), download model + tutoriel, get back to default program
+    secondary.innerHTML = `<h1 class="text-center py-2">Réglages</h1>
+    <div class="container mb-5">
+        <h2 class="py-1">Téléchargement</h2>
+        <p>Le <strong>model.json</strong> est la structure de base du fichier qu'il faut importer.</p>
+        <p>Le <strong>tutoriel.md</strong> est le guide pour remplir ce fichier selon les besoins.</p>
+        <a href="data/model.json" download="model.json">model.json</a>
+        <a href="data/tutorial.md" download="tutoriel.md">tutoriel.md</a>
+    </div>
+    <div class="container mb-5">
+        <h2 class="py-1">Importation</h2>
+        <div class="mb-3">
+          <label for="formFile" class="form-label">Importe le fichier <strong>.json</strong></label>
+          <input class="form-control" type="file" id="formFile">
+        </div>
+    </div>
+    <div class="container mb-5">
+        <h2 class="py-1">Réinitialiser</h2>
+        <p>Pour revenir à la version de base (programme par défaut).</p>
+        <button class="btn btn-primary" onclick="">Reset</button>
+    </div>`;
 }
 
 function displayWorkout(workout, main) {
